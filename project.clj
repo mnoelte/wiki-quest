b5e71f4d20050c3d5187d55acfbeb41695d59d91
mnoelte <noelte@suub.uni-bremen.de>
2014-09-07 11:18:24 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject wiki-quest "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [suub/error-codes "0.2.0"]
+                 [me.raynes/laser "2.0.0-SNAPSHOT"]])
